ea790d291501544d68cc0658ae30d12f87d42a4a
YunSoo Kang <yskang@YunSooui-MacBook-Pro.local>
2015-11-18 11:36:18 +0900

    add textangular and font-awesome

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, mvTicket, $location) {
     $scope.identity = mvIdentity;
     $scope.signin = function (username, password) {
         mvAuth.authenticateUser(username, password).then(function(success) {
