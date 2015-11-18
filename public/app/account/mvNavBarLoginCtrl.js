7436d554c83f9732eb035d8a2ba7f287512b3e75
YunSoo Kang <yskang@YunSooui-MacBook-Pro.local>
2015-11-18 11:59:49 +0900

    add .location in navbar

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,9 +1,10 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, mvTicket, $location) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, mvTicket, $location, $window) {
     $scope.identity = mvIdentity;
     $scope.signin = function (username, password) {
         mvAuth.authenticateUser(username, password).then(function(success) {
             if(success){
                 mvNotifier.notify('You have successfully signed in!');
+                $window.location.reload();
             } else {
                 mvNotifier.notify('Username/Password combination incorrect');
             }
